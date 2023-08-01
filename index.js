35129eb1c9eec19e9f5bd6a81459beddb743298c
Ozioma Uzoegwu <31167303+Tessot@users.noreply.github.com>
2023-08-01 13:28:51 +0100

    first commit new version

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const github = require('@actions/github');
 try{
 
     const nameToGreet = core.getInput('who-to-greet');
-    console.log('Hello $(nameToGreet)!');
+    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput('time', time);
 
     const payload = JSON.stringify(github.context.payload)
-    console.log('The event payload: $(payload)');
+    console.log(`The event payload: ${payload}`);
 
 }catch(error){
     core.setFailed(error.messsage);
